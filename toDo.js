cc704d12ed2d23b4e451c83cbfb36ab8b61ee4b2
Matthew Gouws <matty.gouws@gmail.com>
2015-03-23 20:32:16 +0200

    Added ToDo - Basic Layout of what Functions I think we still need to implement and suggestions on how we should possibly go about implementing them. Comments Welcome

---
+++ toDo.js
@@ -0,0 +1,69 @@
+/********************************************************************************************************/
+function AprraisalNotification(obj)
+{
+	var details = JSON.parse(obj);
+	//Query the MongoDB to find all users with the UserID contained in OBJ
+	//if the user has accepted Appraisal Notification --Default ON
+	//Then
+		//Send
+	//else	
+		//ignore
+	//webNotification(newObj)
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function DeleteNotification(obj)
+{
+	var details = JSON.parse(obj);
+	//check the obj if the user who issued the delete wishes to send a notification
+	//Query the MongoDB to find all users with the UserID contained in OBJ
+	//if the user has accepted Delete Notification -- Default ON
+	//Then
+		//Send
+	//else	
+		//ignore
+	//webNotification(newObj)
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function StandardNotification(obj)
+{
+	var details = JSON.parse(obj);
+	//traverse up the Thread Tree to the root node
+	//for each node check if any users are listed to receive a notification for that specific thread
+	//Send emails for those users who wish to recieve Notifications
+	//webNotification(newObj)
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function WebNotification(obj)
+{
+	var details = JSON.parse(obj);
+	//Add the Notification to a DB to be displayed via the Web - Read -- Default ON
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function EditSubscription(obj)
+{
+	var details = JSON.parse(obj);
+	//Check if the edit is a delete or edit
+	//Perform the edit in the DB (New Thread/Add Users/Remove Users)
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function DailyNotification() //--Extra User can specify the frequency of "daily" emails - Daily-2nd Day- Weekly - Monthly
+{
+	//Use Node Schedule -- https://github.com/node-schedule (CRON Style)
+	//Go Through the Notification Database and compile Email's per user to be sent about Current Day's Happenings
+}
+//--------------------------------------------------------------------------------------------------------
+/********************************************************************************************************/
+function GetWebNotifications(obj) 
+{
+	var details = JSON.parse(obj);
+	//Find all WebNotifications associated with a specific user
+	//Compile into a JSON array
+	//Returns JSON ARRAY
+
+}
+//---------------------------------------------------------------------------------------------------------
